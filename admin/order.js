d76cf147d47923ec57e35a15268b1a1d22cf2fae
Xgjgfdss <119746263+Xgjgfdss@users.noreply.github.com>
2023-03-10 09:05:48 +0300

    Add files via upload

---
+++ admin/order.js
@@ -0,0 +1,38 @@
+const Orders = [
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        producName: 'LARVENDER KF102 Drone Mini Drone',
+        producNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        producName: 'Foldable Mini Drone',
+        producNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+]
